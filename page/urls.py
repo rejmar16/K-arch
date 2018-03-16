65a59f89763e896ec329848f12a927878fea0d26
Martin <rejhonm@gmail.com>
2018-03-16 13:01:31 +0100

    change css

--- page/urls.py
+++ page/urls.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
+#from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     url(r'^$', views.rd_list, name='rd_list'),
@@ -14,3 +16,5 @@ urlpatterns = [
     url(r'^projekty/interiery/$', views.rd_filter_INTER, name='rd_filter_INTER'),
     url(r'^projekty/jine/$', views.rd_filter_JINE, name='rd_filter_JINE')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+#urlpatterns += staticfiles_urlpatterns()
